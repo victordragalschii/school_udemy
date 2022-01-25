5cd90ac774d80deffe03fc8025c25537ff0cb850
Victor <vuctor.drag@gmail.com>
2022-01-25 14:35:54 +0200

    first commit

---
+++ Project/js/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log (number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// let number = 4.6;
+// console.log(4/0);
+
+// ---------Урок 7--------
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя?');
+// answer[1] = prompt('Как ваша фамилия?');
+// answer[2] = prompt('Сколько вам лет?');
+
+// console.log(typeof(answer));
+
+// ---------Урок 8--------
+// const category = 'toys';
+// console.log('https://someurl.com/' + 'category' + '/' + '4'); 
+// console.log(`https://someurl.com/${category}/5`); //Лучше писать вот таким способом
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+// ---------Урок 9--------
+
+// console.log('arr' + '- object');
+// console.log('4' + '- object');
+
+//Чем оличается префиксная форма от постфискной?
+
+
+
+// let incr = 10,
+//     decr = 10;
+
+    // incr++; //постфискная форма
+    // decr--;
+
+    // ++incr; //префиксная форма
+    // --decr;
+
+    // console.log(incr);
+    // console.log(decr);
+
+    // console.log(5%2);
+    // console.log(2 + 2 * 2 !== '8');
+    // && - и
+    // || - или
+
+    // const isChecked = true,
+    //       isClosed = true;
+
+    //       console.log(isChecked && isClosed);
+
+
+// ---------Урок 10--------
